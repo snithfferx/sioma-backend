836203edca2c5ac09bc9e6376ebdc98b65c902c4
JorgeDS <jrivera@piensads.com>
2025-03-22 13:29:49 -0600

    SIOMA | Backend | Token utility

---
+++ src/lib/utils/token.ts
@@ -0,0 +1,19 @@
+import Jwt from "jsonwebtoken";
+import { APP_SECRET, JWT_EXPIRATION } from '@Configs/constants';
+import bcrypt from 'bcryptjs';
+
+export const generateToken = async (payload: {
+    id: number;
+    email: string;
+}) => {
+    if (!APP_SECRET || !JWT_EXPIRATION) {
+        throw new Error('APP_SECRET or JWT_EXPIRATION is not defined');
+    }
+    const secret = await bcrypt.hash(APP_SECRET, 10);
+    return Jwt.sign({
+        sub: payload.id,
+        email: payload.email,
+        iat: Date.now(),
+        exp: Date.now() + 1000 * 60 * 60 * Number(JWT_EXPIRATION) // 1 day
+    }, secret, { expiresIn: Number(JWT_EXPIRATION) });
+}
