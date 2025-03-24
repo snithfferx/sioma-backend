0f2d15e668f9e9bb1c7049d36adb7227cd681d3f
JorgeDS <jrivera@piensads.com>
2025-03-24 14:33:14 -0600

    SIOMA | Backend | Resolving main db merge conflicts

--- db/sqlite/index.ts
+++ db/sqlite/index.ts
@@ -1,13 +1,13 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import * as schema from "@DB/sqlite/schema";
-import "dotenv/config";
+import { DB_NAME, DB_HOST } from "@Configs/constants";
 
-if (!process.env.DATABASE_NAME) {
+if (!DB_NAME || !DB_HOST) {
     throw new Error("Missing Database Configuration");
 }
 
-const db_url = `${process.env.DATABASE_URL}${process.env.DATABASE_NAME}.db`;
+const db_url = `${DB_HOST}${DB_NAME}.db`;
 
 export const client = createClient({ url: db_url || "file:./dev.db" });
 
