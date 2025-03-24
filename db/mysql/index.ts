89d34138e4400cc311377bf5158e5b7b42556cb9
JorgeDS <jrivera@piensads.com>
2025-03-24 14:32:56 -0600

    SIOMA | Backend | Resolving db merge conflicts

--- db/mysql/index.ts
+++ db/mysql/index.ts
@@ -1,15 +1,19 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema.js';
-import 'dotenv/config';
+import { MYSQL_DB, MYSQL_HOST, MYSQL_PASS, MYSQL_USER } from '@Configs/constants';
+
+if (!MYSQL_DB || !MYSQL_HOST || !MYSQL_USER || !MYSQL_PASS) {
+    throw new Error('Missing Database Configuration');
+}
 
 // console.log(process.env.MYSQL_PASS);
 
 const connection = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASS,
-    database: process.env.MYSQL_NAME,
+    host: MYSQL_HOST,
+    user: MYSQL_USER,
+    password: MYSQL_PASS,
+    database: MYSQL_DB,
 });
 
 export const db = drizzle(connection, { schema, mode: 'default' });
