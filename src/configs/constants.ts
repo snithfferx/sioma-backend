94c6e7538f45d94f7a11dfae61060d1ae4405dda
JorgeDS <jrivera@piensads.com>
2025-03-25 14:22:52 -0600

    SIOMA | backend | Updating constants

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
-export const APP_PORT = process.env.APP_PORT;
-export const APP_HOST = process.env.APP_HOST;
+export const APP_PORT = process.env.APP_HOST_PORT;
+export const APP_HOST = process.env.APP_HOST_NAME;
 export const APP_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRATION = process.env.JWT_EXPIRES_IN;
 // MYSQL DB
@@ -15,3 +15,8 @@ export const DB_NAME = process.env.LDB_NAME;
 export const DB_HOST = process.env.LDB_HOST;
 export const DB_USER = process.env.LDB_USER;
 export const DB_PASS = process.env.LDB_PASSWORD;
+// Shopify
+export const SHOPIFY_KEY = process.env.SHOPIFY_API_KEY;
+export const SHOPIFY_SECRET = process.env.SHOPIFY_SECRET_KEY;
+export const SHOPIFY_SHOP = process.env.SHOPIFY_SHOP_NAME;
+export const SHOPIFY_SCOPES = process.env.SHOPIFY_SCOPES;
