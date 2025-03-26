5abc31511dff97fd14d475b033e0141022cbbcff
JorgeDS <jrivera@piensads.com>
2025-03-26 14:20:48 -0600

    SIOMA | Backend | Updating Product Controller

--- src/modules/products/Product.controller.ts
+++ src/modules/products/Product.controller.ts
@@ -1,3 +1,14 @@
+import { ProductModel } from "@Modules/products/Product.model";
+
+const model = new ProductModel
 export class ProductController {
     constructor() { }
+
+    async getAllProducts(terms: string | null, page: number, limit: number) {
+        if (!terms) {
+            return model.getAllProducts(page, limit);
+        } else {
+            return model.getProductsByTerms(terms, page, limit);
+        }
+    }
 }
